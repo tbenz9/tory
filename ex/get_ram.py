8488fc7812fb45961ad4243c92c339702f55ef8a
Justin Walter Walker <walker94@llnl.gov>
2016-06-23 16:42:53 -0700

    updated file

--- ex/get_ram.py
+++ ex/get_ram.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python
+"""
+git pull
+git add <filename>
+git commit -m "message"
+git pull
+git push
+"""
 
 import psutil
+import pprint
 
 totalRam = psutil.virtual_memory()[0]
+freeRam = psutil.virutal_memory()[1]
+ramOS_info{}
 
-print(totalRam)
+
+
+pprint.pprint(ramOS_info)
+
