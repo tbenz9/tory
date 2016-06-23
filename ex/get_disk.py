93b35495183b09a0f1af6e2f4bea03fecd828fbc
Thomas Bennett <tbenz9@gmail.com>
2016-06-23 16:36:50 -0700

    working disk_info

--- ex/get_disk.py
+++ ex/get_disk.py
@@ -4,11 +4,15 @@ import psutil
 import pprint
 
 p = psutil.disk_partitions()
-disk_info{}
+disk_info = {}
 
-for i in p:
-    disk_info[p[0][0]]  <- FILL OUT DICTIONARY
-    DO LOTS OF STUFF
+def get_disk_partitions():
+    count = 0
+    for i in p:
+        disk_info[p[count][0]] = {'name': p[count][0],
+                                  'mount': p[count][1],
+                                  'type': p[count][2]}
+        count += 1
     return disk_info
 
-pprint.pprint(disk_info)
+pprint.pprint(get_disk_partitions())
