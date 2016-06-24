b2ecd720478e9bcb9b591ae6ec63f4e85a542f7a
Justin Walter Walker <walker94@llnl.gov>
2016-06-24 11:13:42 -0700

    Finalized RAM & OS Info.

--- ex/get_ram.py
+++ ex/get_ram.py
@@ -4,25 +4,27 @@ import psutil
 import pprint
 import os
 
+def get_os_vers():
+    f = open("/etc/redhat-release", "r")
+    return f.readline().strip("\n").strip(" ")
+
 def get_ramOS_partitions():
 
 	ram = psutil.virtual_memory()
 
 	ramOS_info = {}
 
-	index = 0
 	for x in ram:
-		total_ram = ram[index]
+		total_ram = ram[0]
 		free_ram = ram[1]
 		swap_space = psutil.swap_memory()[2]
-		os_version = os.system("cat /etc/redhat-release")
 		kernel_version = os.uname()[2]
 		hostname = os.uname()[1]
 
 		ramOS_info = {"total_ram": total_ram,
 			      "free_ram": free_ram, 
 			      "swap_space": swap_space, 
-			      "os_version": os_version,
+			      "os_version": get_os_vers(),
 			      "kernel_version": kernel_version,
 			      "hostname": hostname}
 		
