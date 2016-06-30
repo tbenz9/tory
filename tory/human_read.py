155663a0299e5430611a4b15beccc687e6e923d5
KatieJ16 <katie@acornonsite.com>
2016-06-30 14:14:30 -0700

    ram added to human readable

--- tory/human_read.py
+++ tory/human_read.py
@@ -1,7 +1,7 @@
 import network
 import cpu
 
-#make network human readable
+#make network human readable, dict is a dictionary from the output of get_network_info()
 def net_human(dict):
     for name in dict:
         print "name: " + name
@@ -14,7 +14,8 @@ def net_human(dict):
             print "\t Link: Up"
         print ""
     
-# name cpu infor human readable
+#cpu info in human readable, dict is a dictionary from the output of mch_cpu()
+
 def cpu_human(dict):
     print "in cpu_human()"
     print "Number of CPUs: " + str(dict["num_cpus"])
@@ -25,5 +26,13 @@ def cpu_human(dict):
     print "CPU Speed: " + dict["cpu_speed"]
     print "CPU Serial Number: " + dict["serial_number"]
 
+#disk info in  human readable, dict is a dictionary from the output of get_disj_partitions()
+def ram_human(dict):
+    print "Total RAM: " + str(dict["total_ram"])
+    print "Free RAM: " + str(dict["free_ram"])
+    print "Swap Space: " + str(dict["swap_space"])
+    print "OS Version: " + dict["os_version"]
+    print "Kernel Version: " + dict["kernel_version"]
+    print "Hostname: " + dict["hostname"]
 
 
