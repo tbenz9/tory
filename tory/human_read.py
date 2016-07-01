5430786b7fbbfa3a1e36afd856614d12359fbdee
KatieJ16 <katie@acornonsite.com>
2016-07-01 10:57:00 -0700

    added user to human readable

--- tory/human_read.py
+++ tory/human_read.py
@@ -26,7 +26,7 @@ def cpu_human(dict):
     print "CPU Speed: " + dict["cpu_speed"]
     print "CPU Serial Number: " + dict["serial_number"]
 
-#disk info in  human readable, dict is a dictionary from the output of get_disj_partitions()
+#disk info in  human readable, dict is a dictionary from the output of get_disk_partitions()
 def ram_human(dict):
     print "Total RAM: " + str(dict["total_ram"])
     print "Free RAM: " + str(dict["free_ram"])
@@ -34,5 +34,14 @@ def ram_human(dict):
     print "OS Version: " + dict["os_version"]
     print "Kernel Version: " + dict["kernel_version"]
     print "Hostname: " + dict["hostname"]
+
+#user info in human readable, dict is a dictionary from teh output of get_users()
+def users_human(dict):
+    print "Current Users: ",
+    print ', '.join(dict['current_users'])
+    print "\nAll Users:"
+    print '%-12s\t%-12s' % ('Name:', 'Group:')
+    for name in dict['groups']:
+        print '%-12s\t%-12s' % (name, dict['groups'][name])
 
 
