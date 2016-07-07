a3335474b4b079415b41e1899ee047bc7e902b14
walker94 <walker94@llnl.gov>
2016-07-07 13:18:15 -0700

    All information with simple status works, just need to make it human readable

--- tory/simple.py
+++ tory/simple.py
@@ -5,22 +5,24 @@ import pprint
 import network
 import cpu
 import os
+import socket
+from network import get_network_info
 
 def get_simple_info():
-	
+
 	simple_info = {}
 	
 	cpu_count = psutil.cpu_count()
 	ram = psutil.virtual_memory()
-	network = psutil.net_if_addrs()
+	network = socket.gethostbyname(socket.gethostname())
 	disk = psutil.disk_usage('/')
 
-	for x in simple:
+	for x in network:
 		num_cpu = cpu_count
 		total_ram = ram[0]
 		#ip_address = get_network_info(addresses[0][1])
 		#ip_address = get_network_info("ip_address")
-		ip_address = network[0][1]
+		ip_address = network
 		total_disk = disk[0]
 		hostname = os.uname()[1]
 
