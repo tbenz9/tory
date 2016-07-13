816ccfdb635f516ebe807f02f6338a786d0a5556
walker94 <walker94@llnl.gov>
2016-07-13 15:16:39 -0700

    addition of extra comments and organizational structure

--- tory/ram.py
+++ tory/ram.py
@@ -7,15 +7,12 @@ import os.path
 from human_read import find_units
 
 def get_os_vers(): # defined function to return OS Version from command
-    
-    
-
-# testing out error checking 
-	if os.path.exists("/etc/redhat-release"):
+     
+	if os.path.exists("/etc/redhat-release"): # error checking, checking to see if OS version is found within file
 		f = open("/etc/redhat-release", "r")
         	return f.readline().strip("\n").strip(" ")
 	else:
-		print "There is no such file"
+		print "There is no such file" # if file is not found
 
 def get_ram_partitions(): # defined function to get all the RAM info
 
